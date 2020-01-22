21d1ee865ffedae57130652dec8df3e37482a475
linnlinnhtet <linnlinnhtet115@gmail.com>
2020-01-22 00:40:38 -0800

    Initial author routes

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 //Import index route from router folder
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 //Ejs Setup
 app.set('view engine', 'ejs');
@@ -15,6 +17,7 @@ app.set('views', __dirname + '/views');
 app.set('layouts', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 //Mongoose connection
 const mongoose = require('mongoose');
@@ -28,10 +31,11 @@ const mongoose = require('mongoose');
 // db.once('open', () => {
 //     console.log("Connected to mongoose");
 // })
-mongoose.connect('mongodb+srv://MyMongoDBUser:MyMongoDBUser1995@gettingstarted-v1ey5.mongodb.net/test', {
+mongoose.connect('mongodb+srv://MyMongoDBUser:MyMongoDBUser1995@gettingstarted-v1ey5.mongodb.net/mybrary', {
     useNewUrlParser: true
 });
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
